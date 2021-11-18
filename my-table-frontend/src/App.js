1705939d14d863ac6b07637f1c5be8e6884c4ce3
sijang0812 <sijang0812@gmail.com>
2021-11-18 23:55:23 +0900

    APPLY MAIN PAGE

--- my-table-frontend/src/App.js
+++ my-table-frontend/src/App.js
@@ -24,21 +24,46 @@ function App() {
 
 export default App;
 */
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from "react";
+//import {useEffect, useState} from "react";
 
 function App() { 
+  /*
   const [message, setMessage] = useState([]);
   useEffect(() => { 
     fetch("/main").then((response) => { return response.json(); }).then(function (data) { setMessage(data); }); }, []);
-    
+  */
+    /*
     return ( <div className="App"> 
-             <header className="App-header">
-             <img src={logo} className="App-logo" alt="logo"/>
-             <p> Edit <code>src/App.js</code> and save to reload. </p> 
-             <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" > Learn React </a> 
-             <ul> {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)} </ul> </header> </div> 
+                <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p> Edit <code>src/App.js</code> and save to reload. </p> 
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" > Learn React </a> 
+                <ul> {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)} </ul> </header> 
+             </div> 
+           ); 
+    */
+    return (  /*
+              <div className="App"> 
+                <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p> Edit <code>src/App.js</code> and save to reload. </p> 
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" > Learn React </a> 
+                <ul> {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)} </ul> </header> 
+              </div> 
+              */
+              <div className="home" id="bodyHome">
+                <div className="home" id="divHome">
+                  <img className="imgLogo" id="imgHomeLogo" src="/src/img/homeLogo.png" alt="로고" />
+                  <div id="divHomeDlvryBtn">
+                    <img id="imgHomeDlvryBtn" src="img/homeDlvryBtn.png" alt="배달주문" />
+                  </div>
+                  <div id="divHomeRstrntBtn">
+                    <img id="imgHomeRstrntBtn" src="img/homeRstrntBtn.png" alt="레스토랑예약" />
+                  </div>
+                </div>
+              </div>
            ); 
 } 
 
