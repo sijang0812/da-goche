298ca31669d239e021af64a7b3d09ecb6cd00c4f
sijang0812 <sijang0812@gmail.com>
2021-11-20 23:44:22 +0900

    APPLY PAGING FUNCTION TO MAIN PAGE

--- my-table-frontend/src/App.js
+++ my-table-frontend/src/App.js
@@ -25,16 +25,19 @@ function App() {
 export default App;
 */
 //import logo from './logo.svg';
+//import React, { useState } from "react";
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 import homeLogo from './img/homeLogo.png';
 import homeDlvryBtn from './img/homeDlvryBtn.png';
 import homeRstrntBtn from './img/homeRstrntBtn.png';
+import main from "./App"
+import delivery from "./Delivery"
+import restaurant from "./App"
 import './App.css';
+import { Routes } from "react-router";
 //import {useEffect, useState} from "react";
 
 function App() { 
-  fnMoveMage() {
-  
-  }
   /*
   const [message, setMessage] = useState([]);
   useEffect(() => { 
@@ -50,17 +53,33 @@ function App() {
              </div> 
            ); 
     */
-    return ( <div className="home" id="bodyHome">
+    return (
+            <Router> 
+              <div className="home" id="bodyHome">
                 <div className="home" id="divHome">
-                  <img className="imgLogo" id="imgHomeLogo" src={homeLogo} alt="로고" />
+                  <Link to="/Main">
+                    <img className="imgLogo" id="imgHomeLogo" src={homeLogo} alt="로고" />
+                  </Link>
                   <div id="divHomeDlvryBtn">
-                    <img id="imgHomeDlvryBtn" src={homeDlvryBtn} alt="배달주문" oncClick={fnMoveMage}/>
+                    <Link to="/delivery">
+                      <img id="imgHomeDlvryBtn" src={homeDlvryBtn} alt="배달주문" />
+                    </Link>
                   </div>
                   <div id="divHomeRstrntBtn">
-                    <img id="imgHomeRstrntBtn" src={homeRstrntBtn} alt="레스토랑예약" />
+                    <Link to="/restaurant">
+                      <img id="imgHomeRstrntBtn" src={homeRstrntBtn} alt="레스토랑예약" />
+                    </Link>
                   </div>
                 </div>
-             </div>
+              </div>
+              <div>
+                <Routes>
+                  <Route exact path="/" componect={main} />
+                  <Route path="/delivery" componect={delivery} />
+                  <Route path="/restaurant" componect={restaurant} />
+                </Routes>
+              </div>
+             </Router>
            ); 
 } 
 
