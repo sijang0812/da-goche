233f762dc6f65ba5b7b9ff8e7ab99664eb40e7d5
sijang0812 <sijang0812@gmail.com>
2021-11-24 00:31:21 +0900

    COMPLETELY APPLY PAGING FUNCTION TO MAIN PAGE

--- my-table-frontend/src/App.js
+++ my-table-frontend/src/App.js
@@ -1,13 +1,20 @@
-import React,{Component} from 'react';
-import Routers from './Router'; 
-class App extends Component {
-  render() {
-      return (
-        <>
-        <Routers/>
-        </>
-      );
-  }
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './Main';
+import Delivery from './Delivery';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/Delivery' element={<Delivery />} />
+          <Route path='/Main' element={<Main />} />
+          <Route render={() => <div className='error'>에러 페이지</div>} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- my-table-frontend/src/Main.js
+++ my-table-frontend/src/Main.js
@@ -1,86 +1,23 @@
-/*import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-*/
-//import logo from './logo.svg';
-//import React, { useState } from "react";
-import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 import homeLogo from './img/homeLogo.png';
 import homeDlvryBtn from './img/homeDlvryBtn.png';
 import homeRstrntBtn from './img/homeRstrntBtn.png';
-import Main from "./App"
-import Delivery from "./Delivery"
-import Restaurant from "./App"
-import './App.css';
-import { Routes } from "react-router";
-import {useEffect, useState} from "react";
 
-function Main() { 
-  
-  const [message, setMessage] = useState([]);
-  useEffect(() => { 
-    fetch("/Main").then((response) => { return response.json(); }).then(function (data) { setMessage(data); }); }, []);
-  
-    /*
-    return ( <div className="App"> 
-                <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p> Edit <code>src/App.js</code> and save to reload. </p> 
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" > Learn React </a> 
-                <ul> {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)} </ul> </header> 
-             </div> 
-           ); 
-    */
-    return (
-            <Router> 
-              <div className="home" id="bodyHome">
-                <div className="home" id="divHome">
-                  <Link to="/Main">
-                    <img className="imgLogo" id="imgHomeLogo" src={homeLogo} alt="로고" />
-                  </Link>
-                  <div id="divHomeDlvryBtn">
-                    <Link to="/Delivery">
-                      <img id="imgHomeDlvryBtn" src={homeDlvryBtn} alt="배달주문" />
-                    </Link>
-                  </div>
-                  <div id="divHomeRstrntBtn">
-                    <Link to="/Restaurant">
-                      <img id="imgHomeRstrntBtn" src={homeRstrntBtn} alt="레스토랑예약" />
-                    </Link>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <Routes>
-                  <Route exact path="/" componect={Main} />
-                  <Route path="/Delivery" componect={Delivery} />
-                  <Route path="/Restaurant" componect={Restaurant} />
-                </Routes>
-              </div>
-             </Router>
-           ); 
-} 
+function Main() {
+  return (
+    <div className="home" id="bodyHome">
+      <div className="home" id="divHome">
+        <img className="imgLogo" id="imgHomeLogo" src={homeLogo} alt="로고" />
+        <div id="divHomeDlvryBtn">
+          <img id="imgHomeDlvryBtn" src={homeDlvryBtn} alt="배달주문" />
+        </div>
+        <div id="divHomeRstrntBtn">
+          <img id="imgHomeRstrntBtn" src={homeRstrntBtn} alt="레스토랑예약" />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Main;
