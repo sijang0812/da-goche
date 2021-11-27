d4406308e8958dddfe7a04bbb858728b501d0839
sijang0812 <sijang0812@gmail.com>
2021-11-27 23:59:16 +0900

    temp

--- my-table-frontend/src/Delivery.js
+++ my-table-frontend/src/Delivery.js
@@ -1,19 +1,16 @@
-import logo from './logo.svg';
+import Head from './Head.js';
 import './App.css';
 import React from 'react';
+
 
 function Delivery() { 
   // const [message, setMessage] = useState([]);
   // useEffect(() => { 
   //   fetch("/Delivery").then((response) => { return response.json(); }).then(function (data) { setMessage(data); }); }, []);
     return ( 
-      <div className="App"> 
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p> Edit <code>src/App.js</code> and save to reload. </p> 
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" > Learn React </a> 
+      <div> 
         {/* <ul> {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)} </ul> */}
-        </header>
+        <Head />
       </div> 
     ); 
 }
--- my-table-frontend/src/Head.js
+++ my-table-frontend/src/Head.js
@@ -1,9 +1,13 @@
-import React from 'react';
+// eslint-disable-next-line
 import { Link } from 'react-router-dom';
+import React from 'react';
+import './App.css';
+import homeLogo from ;
 
 function Head() {
     return (
-        <>
+        <div>
+            <img className="imgLogo" id="imgHomeLogo" src={homeLogo} alt="로고" />
             <Link to="/main/restaurant">
                 <button>레스토랑예약</button>
             </Link>
@@ -13,7 +17,7 @@ function Head() {
             <Link to="/main">
                 <button>메인</button>
             </Link>
-        </>
+        </div>
     );
 }
 
