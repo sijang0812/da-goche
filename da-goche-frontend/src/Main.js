beb14bfee3c7a9feaded9196fe88e28236a2a54a
sijang0812 <sijang0812@gmail.com>
2021-12-06 23:14:21 +0900

    COMPLETE POP UP MESSAGE AFTER CLICKING LOGO IN MAIN PAGE

--- da-goche-frontend/src/Main.js
+++ da-goche-frontend/src/Main.js
@@ -1,3 +1,4 @@
+/*
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Question from './Question';
@@ -28,6 +29,31 @@ export default function Main({ categories }) {
             })}
           </div>
           }
+        </div>
+      </div>
+    </div>
+  )
+}
+*/
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Question from './Question';
+// import homeLogo from './img/homeLogo.png';
+// import homeDlvryBtn from './img/homeDlvryBtn.png';
+// import homeRstrntBtn from './img/homeRstrntBtn.png';
+
+export default function Main({ categories }) {
+  const [depth, setDepth] = useState(1);
+
+  return (
+    <div className="bg">
+      <div className="divLogo">
+        <p className="logo">DA GOCHE</p>
+        <p id="pQstn">수리할 장소는 어디입니까?</p>
+        <div onClick={() => setDepth(2)}>
+          {categories.map( category => {
+            return <Question category={category} key={category.id} />
+          })}
         </div>
       </div>
     </div>
--- da-goche-frontend/src/Question.js
+++ da-goche-frontend/src/Question.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 export default function Question({ category }) {
+    function clickLogo(param) {
+        //alert(`hear: ${param}`);
+        alert(`${param}`);
+        
+    }
+
     return (
         <div className="divAnswr">
-            <img className="imgAnswr" src={require( './img/' +  category.id + '.png').default} />
+            <img className="imgAnswr" src={require( './img/' +  category.id + '.png').default } onClick={() => clickLogo(category.name)} />
             <p className="pAnswr">{category.name}</p>
         </div>
     )
