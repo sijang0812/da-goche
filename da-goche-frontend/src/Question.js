7a5c4723b8c21d78eb52b49b7be090b2ffa278c3
sijang0812 <sijang0812@gmail.com>
2021-12-07 23:43:22 +0900

    TEMPORARY COMMIT

--- da-goche-frontend/src/Question.js
+++ da-goche-frontend/src/Question.js
@@ -9,7 +9,8 @@ export default function Question({ category }) {
 
     return (
         <div className="divAnswr">
-            <img className="imgAnswr" src={require( './img/' +  category.id + '.png').default } onClick={() => clickLogo(category.name)} />
+            {/* <img className="imgAnswr" src={require( './img/' +  category.id + '.png').default } onClick={() => clickLogo(category.sub[0].id)} /> */}
+            <img className="imgAnswr" src={require( './img/' +  category.id + '.png').default } onClick={() => clickLogo(category.sub)} />
             <p className="pAnswr">{category.name}</p>
         </div>
     )
