50074a3664cc0c23a4c90b45057aeb66ee3eafa7
sijang0812 <sijang0812@gmail.com>
2021-12-13 01:22:29 +0900

    APPLY FUNCTION ONCLICK TO METHOD .MAP() IN MAIN PAGE2

--- da-goche-frontend/src/Main.js
+++ da-goche-frontend/src/Main.js
@@ -1,85 +1,22 @@
-/*
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Question from './Question';
-// import homeLogo from './img/homeLogo.png';
-// import homeDlvryBtn from './img/homeDlvryBtn.png';
-// import homeRstrntBtn from './img/homeRstrntBtn.png';
-
-export default function Main({ categories }) {
-  const [depth, setDepth] = useState(1);
-
-  return (
-    <div className="bg">
-      <div className="divLogo">
-        <p className="logo">DA GOCHE</p>
-        <p id="pQstn">수리할 장소는 어디입니까?</p>
-        <div onClick={() => setDepth(2)}>
-          {depth === 1
-          ?   
-          <div>
-            {categories.map(category => {
-              return <Question category={category} key={category.id}/>
-            })}
-          </div>
-          :
-          <div>
-            {categories.map(category => {
-              return <Question category={category} key={category.id}/>
-            })}
-          </div>
-          }
-        </div>
-      </div>
-    </div>
-  )
-}
-*/
-/*
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Question from './Question';
-import Main2 from './Main2';
-
-export default function Main({ categories }) {
-  const [depth, setDepth] = useState(1);
-
-  function clickLogo(param) {
-    
-  }
-
-  return (
-    <div className="bg">
-      <div className="divLogo">
-        <p className="logo">DA GOCHE</p>
-        <p id="pQstn">수리할 장소는 어디입니까?</p>
-        <div onClick={clickLogo}>
-          {categories.map( category => {
-            return <Question category={category} key={category.id} onClick = {() => clickLogo(category.sub)}/>
-          })}
-        </div>
-      </div>
-    </div>
-  )
-}
-*/
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Question from './Question';
 
 export default function Main({categories}) {
-  const [logos, setLogos] = useState({categories});
+  const [logos, setLogos] = useState({categories}.categories);
   const [cntClick, setCntClick] = useState(0);
-
-  console.log(logos);
   
   function clickLogo(param) {
-    setCntClick(1);
-    //cntClick == 1 ? alert(); : alert(cntClick);
-    setLogos(param);
-    console.log(cntClick);
-    console.log(logos);
-    alert(param);
+    const temp = cntClick + 1;
+    console.log("temp", temp);
+    setCntClick(temp);
+    console.log("cntClick", cntClick);
+    console.log("logos", logos);
+    if(cntClick === 1) {
+      setLogos(param);
+    } else {
+      alert("메인"); 
+    }
   }
 
   return (
@@ -87,17 +24,17 @@ export default function Main({categories}) {
       <div className="divLogo">
         <p className="logo">DA GOCHE</p>
         <p id="pQstn">수리할 장소는 어디입니까?</p>
-        <div>
-          {logos.categories.map( 
+          {logos.map( 
             category => {
               return (
-                <div className="divAnswr" onClick={() => clickLogo("abc")}>
-                  <Question category = {category} key = {category.id} />
+                <div className="divAnswr" key = {category.id}>
+                  {/* <Question category = {category} key = {category.id} /> */}
+                  <img className="imgAnswr" src={require( './img/' +  category.id + '.png').default } onClick={() => clickLogo(category.sub)}/>
+                  <p className="pAnswr">{category.name}</p>
                 </div>
               )
             }
           )}
-        </div>
       </div>
     </div>
   )
