a63b0cc1123fadd0db7d63effd79d27b9d3e0e0f
sijang0812 <sijang0812@gmail.com>
2021-12-21 23:57:17 +0900

    TEMP3

--- da-goche-frontend/src/List.js
+++ da-goche-frontend/src/List.js
@@ -1,6 +1,8 @@
+/*
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+
 import Head from './Head';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -28,13 +30,63 @@ export default function Main({selectId}) {
       <Head/>
       <div className="divBody">
         {/* <p id="pQstn">{selectId}</p>
-        <p id="pQstn">{list}</p> */}
+        <p id="pQstn">{list}</p> }
         <table>
-          <c:forEach items="${list}">
+          ${list}.
+
             <tr>
           
             </tr>
-          </c:forEach>
+        </table>
+      </div>
+    </div>
+  )
+}
+*/
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+import Head from './Head';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+export default function Main({selectId}) {
+  const [list, setList] = useState();
+  
+  function callback(param) {
+    setList(param);
+    console.log("[list]: " + list);
+  }
+
+  useEffect(
+    () => {
+      axios({
+        srcId  : {selectId},
+        url    : '/list',
+        method : 'GET'
+      }).then((res) => {
+        callback(res.data);
+      })
+    }, []
+  );
+
+  return (
+    <div className="bg">
+      <Head/>
+      <div className="divBody">
+        {/* <p id="pQstn">{selectId}</p>
+        <p id="pQstn">{list}</p> */}
+        <table>
+          {list.map( 
+            item => {
+              return (
+                <tr>
+                  <td>{item.tlsId}</td>
+                  <td>{item.tlsNm}</td>
+                </tr>
+              )
+            }
+          )}
         </table>
       </div>
     </div>
