023e99bd4e0b163ba4cca56e320ce3eabdf32d7e
sijang0812 <sijang0812@gmail.com>
2022-01-12 23:21:54 +0900

    TEMP

--- da-goche-frontend/src/List.js
+++ da-goche-frontend/src/List.js
@@ -52,36 +52,33 @@ import Item from './Item';
 
 export default function Main({selectId}) {
   console.log({selectId});
-  //const [list, setList] = useState([]);
+  const [cntClick, setCntClick] = useState(0);
   let list = [{ tlsId : 'test', tlsNm : '테스트'}];
-  console.log("**list0**" + list[0].tlsNm);
   const url = "http://localhost:3000/list";
-  list = [{ tlsId : 'testa', tlsNm : '테스트a'}];
-  console.log("**list**" + list);
+  console.log("**list0**" + list);
   console.log("**list0**" + list[0].tlsNm);
   axios.get(url)
   .then(function(response){
-    list = [{ tlsId : 'testb', tlsNm : '테스트b'}];
-    console.log("**list**" + list);
-    console.log("**list0**" + list[0].tlsNm);
+    setCntClick(cntClick + 1);
     console.log("성공");
     list = response.data; //setList(response.data);
     //setList(JSON.stringify(response.data));
-    console.log("**list**" + list);
-    console.log("**list0**" + list[0].tlsNm);
+    console.log("**list1**" + list);
+    console.log("**list1**" + list[0].tlsNm);
   })
   .catch(function(error){
     console.log("실패");
   })
-
   return (
     <div className="bg">
       <Head/>
       <div className="divBody">
         {list.map( 
-          item => (
+          item => {
+            return (
             <Item item = {item} key={item.tlsId} />
             )
+          }
         )}
       </div>
     </div>
