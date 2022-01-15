10cc2c09ba233855739733440141bce8654ae103
sijang0812 <sijang0812@gmail.com>
2022-01-15 23:58:55 +0900

    TEMP1

--- da-goche-frontend/src/Item.js
+++ da-goche-frontend/src/Item.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function Item({ item }) {    
-    console.log("테스트",item);
+    console.log("테스트 ",item);
     return (
         <div>
             {/* <img className="imgAnswr" src={require( './img/' +  item.tlsId + '.png').default } /> */}
