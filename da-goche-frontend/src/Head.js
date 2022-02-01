26df5a7f9ec97f654e86a3621c729aefd86f6170
sijang0812 <sijang0812@gmail.com>
2022-02-01 20:49:37 +0900

    TEMP3

--- da-goche-frontend/src/Head.js
+++ da-goche-frontend/src/Head.js
@@ -7,11 +7,42 @@ import Main from './Main';
 
 function Head() {
     return (
-        <Link to="/main">
-            <div className="divLogo">
-                <p className="logo">DA GOCHE</p>
+        <div>
+            <title>DA-GOCHE navigation</title>
+            <Link to="/main">
+                <div className="divLogo">
+                    <p className="logo">DA GOCHE</p>
+                </div>
+            </Link>
+            <div className="divTopMenu">
+                <ul>
+                    <li>전체 카테고리</li>
+                    <ul>
+                        <Link to="/bathroom">
+                        <li>욕실</li>
+                        </Link>
+                        <Link to="/bathroom">
+                        <li>주방</li>
+                        </Link>
+                        <Link to="/bathroom">
+                        <li>거실</li>
+                        </Link>
+                        <Link to="/bathroom">
+                        <li>다용도실/베란다</li>
+                        </Link>
+                        <Link to="/bathroom">
+                        <li>방/침실</li>
+                        </Link>
+                        <Link to="/bathroom">
+                        <li>마당/정원</li>
+                        </Link>
+                        <Link to="/bathroom">
+                        <li>기타</li>
+                        </Link>
+                    </ul>
+                </ul>
             </div>
-        </Link>
+        </div>
     );
 }
 
